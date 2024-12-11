ccd52207e3d25855cdf6e51c3275771161c89a22
Ben Ng <benng512@gmail.com>
2024-12-11 21:58:47 +0800

    20241210

--- demo-helloworld/src/DemoString2.java
+++ demo-helloworld/src/DemoString2.java
@@ -1,3 +1,32 @@
 public class DemoString2 {
-  
+  public static void main(String[] args){
+    String name = "Ben";
+    String x = "LOL";
+    String y ="Eat Sleep Play Recycle";
+    int age = 29;
+    int height = 181;
+    double salary = 4000;
+    char gender = 'M';
+    //----------End of raw data-------------
+    System.out.println("Name: " + name);
+    char nameLastChar = name.charAt(name.length()-1);
+    System.out.println("nameLastChar = " + nameLastChar);
+    // gender
+    boolean isMale = gender == 'M';
+    System.out.println("Is Male?:" + isMale);
+    // rich or poor
+    if (salary > 300_000) {
+      System.out.println("Ben is rich");
+    } else if (salary > 30000){
+      System.out.println("Ben's salary is ok la");
+    } else {
+      System.out.println("Poor Ben");
+    }
+    // 6" tall
+    boolean height6Inch = (height >= 183.0);//false
+    System.out.println("Ben is 6 feet tall? : " + height6Inch);
+
+
+
+  }
 }
